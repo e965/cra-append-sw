d9063d1d6297d23d7dba2e3c2a9ca4ab80e96994
Dmitry <dmitry@cojam.ru>
2020-08-24 15:06:40 +0500

    2.8.0

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # cra-append-sw
 
-  Utility tool to append custom code to ServiceWorker created by [Create React App](https://github.com/facebookincubator/create-react-app). 
-  
-  It allows to keep the default CRA configuration (no ejecting). It simply appends custom code to the ServiceWorker file created by CRA build scripts. By default, it bundles the code using very basic Webpack+Babel configuration (this can be omitted; see options). 
+Fork of [cra-append-sw](https://github.com/tszarzynski/cra-append-sw), but with Typescript files support. See below.
+
+Utility tool to append custom code to ServiceWorker created by [Create React App](https://github.com/facebookincubator/create-react-app).
+
+It allows to keep the default CRA configuration (no ejecting). It simply appends custom code to the ServiceWorker file created by CRA build scripts. By default, it bundles the code using very basic Webpack+Babel configuration (this can be omitted; see options).
 
 ## Installation
 
-    $ npm install cra-append-sw --save
+    $ npm install @e965/cra-append-sw --save
 
 ## Usage
 
@@ -21,13 +23,26 @@
 
 #### Output modes
 
-- `dev` creates `public/<file>` instead of appending the code to `build/service-worker.js`
-- `build` creates `build/<file>` instead of appending the code to `build/service-worker.js`
-- `replace` simply replaces `build/service-worker.js`
+-   `dev` creates `public/<file>` instead of appending the code to `build/service-worker.js`
+-   `build` creates `build/<file>` instead of appending the code to `build/service-worker.js`
+-   `replace` simply replaces `build/service-worker.js`
 
 ## Usage with Create React App
 
-  In `package.json` modify build script to:
+In `package.json` modify build script to:
 
     "start": "react-scripts start && cra-append-sw --mode dev ./custom-sw.js",
     "build": "react-scripts build && cra-append-sw ./custom-sw.js",
+
+## Usage with Typescript
+
+The file is compiled based on your `tsconfig.json` configuration. Please set these values for fields `isolatedModules` and `noEmit`:
+
+```js
+{
+    "compilerOptions": {
+        "isolatedModules": false,
+        "noEmit": false
+    }
+}
+```
---
+++ index.mjs
@@ -0,0 +1,192 @@
+#!/usr/bin/env node
+import fs from 'fs';
+import path from 'path';
+import MemoryFs from 'memory-fs';
+import webpack from 'webpack';
+import Dotenv from 'dotenv-webpack';
+import TerserPlugin from 'terser-webpack-plugin';
+import program from 'commander';
+
+const BUILD_SW_FILE_PATH = 'build/service-worker.js';
+const BUNDLE_FILE_NAME = 'bundle.js';
+
+/**
+ * Command line options
+ */
+program
+    .arguments('<file>')
+    .option('-s, --skip-compile', 'skip compilation')
+    .option('-e, --env [path]', 'path to environment variables files [./.env]', './.env')
+    .option('-m, --mode <mode>', 'output mode [dev|build|replace]', /^(dev|build|replace)$/i)
+    .action(function (file) {
+        if (program.mode === 'dev') {
+            process.env.BABEL_ENV = 'development';
+            process.env.NODE_ENV = 'development';
+        } else {
+            process.env.BABEL_ENV = 'production';
+            process.env.NODE_ENV = 'production';
+        }
+
+        if (program.skipCompile) {
+            read(file).then(result => append(result, file));
+        } else {
+            compile(file).then(({ result, stats }) => append(result, file));
+        }
+    })
+    .parse(process.argv);
+
+/**
+ * Compile entry file using WebPack
+ *
+ * @param {String} entry Path to entry file
+ * @returns {Promise}
+ */
+function compile(entry) {
+    const commonExclude = /(node_modules|bower_components)/;
+
+    const compiler = webpack({
+        mode: program.mode === 'dev' ? 'development' : 'production',
+        entry: [entry],
+        output: {
+            filename: BUNDLE_FILE_NAME,
+            path: '/',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    exclude: new RegExp(commonExclude),
+                    use: 'ts-loader',
+                },
+                {
+                    test: /\.js$/,
+                    exclude: new RegExp(commonExclude),
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    'react-app',
+                                    {
+                                        targets: {
+                                            browsers: ['defaults'],
+                                        },
+                                    },
+                                ],
+                            ],
+                            plugins: ['@babel/plugin-transform-runtime'],
+                        },
+                    },
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        plugins: [
+            new Dotenv({
+                path: program.env,
+                safe: false,
+                silent: true,
+                systemvars: true,
+                expand: true,
+            }),
+        ],
+        optimization: {
+            minimize: true,
+            minimizer: [new TerserPlugin()],
+        },
+    });
+
+    compiler.outputFileSystem = new MemoryFs();
+
+    return new Promise((resolve, reject) => {
+        compiler.run((err, stats) => {
+            if (err) return reject(err);
+
+            if (stats.hasErrors() || stats.hasWarnings()) {
+                return reject(
+                    new Error(
+                        stats.toString({
+                            errorDetails: true,
+                            warnings: true,
+                        })
+                    )
+                );
+            }
+
+            const result = compiler.outputFileSystem.data[BUNDLE_FILE_NAME].toString();
+            resolve({ result, stats });
+        });
+    });
+}
+
+/**
+ * Read entry file
+ *
+ * @param {String} entry Path to entry file
+ * @returns {Promise}
+ */
+function read(entry) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(entry, 'utf8', (error, result) => {
+            if (error) {
+                reject(error);
+            }
+
+            resolve(result);
+        });
+    });
+}
+
+/**
+ * Append custonm code to exisitng ServiceWorker
+ *
+ * @param {String} code
+ * @returns {Promise}
+ */
+function append(code, file) {
+    if (program.mode === 'dev') {
+        const filename = path.basename(file);
+        return writeFile(code, `public/${filename}`);
+    } else if (program.mode === 'build') {
+        const filename = path.basename(file);
+        return writeFile(code, `build/${filename}`);
+    } else if (program.mode === 'replace') {
+        const filename = path.basename(file);
+        return writeFile(code, BUILD_SW_FILE_PATH);
+    } else {
+        // Append to "build/service-worker.js"
+        return new Promise((resolve, reject) => {
+            // Read exisitng SW file
+            fs.readFile(BUILD_SW_FILE_PATH, 'utf8', (error, data) => {
+                if (error) {
+                    reject(error);
+                }
+
+                // append custom code
+                const result = data + code;
+
+                // Write modified SW file
+                fs.writeFile(BUILD_SW_FILE_PATH, result, 'utf8', error => {
+                    if (error) {
+                        reject(error);
+                    }
+
+                    resolve();
+                });
+            });
+        });
+    }
+}
+
+function writeFile(content, file) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, content, 'utf8', error => {
+            if (error) {
+                reject(error);
+            }
+            resolve();
+        });
+    });
+}
